f6b22d5b0173e1836843c50fc24e33305608af13
dgold <golddaniiil13@gmail.com>
2024-11-28 12:29:43 +0300

    Delete backend directory

--- backend/init_db.py
+++
@@ -1,8 +0,0 @@
-from backend.database import init_db
-from backend.models import Base
-from backend.database import engine
-
-init_db()#инициализация базы данных
-print("Database initialized successfully.")
-
-Base.metadata.create_all(bind=engine)#создание всех таблиц
--- backend/models_base.py
+++
@@ -1,3 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()#базовый класс для всех моделей
